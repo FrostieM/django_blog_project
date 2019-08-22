9ea675a652461240dc1d7905ceeeb27fffc15164
frostieM <osinov.maks@gmail.com>
2019-08-23 01:16:02 +0300

    upload

--- static/general/js/blog.js
+++ static/general/js/blog.js
@@ -7,16 +7,13 @@ $(document).ready(function () {
 
     $('.icon').each(function () {
         $(this).on('click', function () {
-            if ($(this).hasClass('current'))
+            let currentTag = $(this);
+            if (currentTag.hasClass('current'))
                 return;
 
-            $('.current').each(function () {
-                $(this).removeClass('current');
-                $(this).find('path').attr('fill', 'white');
-            })
+            $('.current').removeClass('current');
+            currentTag.addClass('current');
 
-            $(this).addClass('current');
-            $(this).find('path').attr('fill', '#6dbad6');
         })
     });
 
@@ -25,4 +22,31 @@ $(document).ready(function () {
             $(this).toggleClass('active');
         })
     });
+
+    $('.view').each(function () {
+        $(this).on('click', function(){
+            $('.view').removeClass('active');
+            $(this).addClass('active');
+
+            let cards = $('.card');
+            cards.toggleClass('flex-row');
+            cards.toggleClass('flex-wrap');
+            cards.toggleClass('col-sm-4');
+        });
+    });
+
+    $('.like').each(function () {
+       $(this).on('click', function () {
+           let currentTag = $(this);
+           let childTag = currentTag.find('small');
+
+           if (currentTag.hasClass('active'))
+               childTag.text(childTag.text() - 1);
+           else
+               childTag.text( Number(childTag.text()) + 1);
+
+           currentTag.toggleClass('active');
+       })
+    });
+
 });
